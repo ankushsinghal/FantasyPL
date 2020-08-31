708974990de3a8c864a86f943d0afe2b59247392
Ankush Singhal <ankushsinghal1995@gmail.com>
2020-08-31 19:45:17 +0530

    Created a Main.py file to call the FantasyPL class methods

---
+++ Main.py
@@ -0,0 +1,8 @@
+from FantasyPL import FantasyPL
+
+fantasyPL = FantasyPL()
+fantasyPL.getFPLJson()
+fantasyPL.createElementTypes()
+fantasyPL.createTeams()
+fantasyPL.createPlayers()
+fantasyPL.predictSquad()
