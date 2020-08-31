471f0c28b604586e0bc5ceade9772b90ab276971
Ankush Singhal <ankushsinghal1995@gmail.com>
2020-09-01 00:06:22 +0530

    Added sortBy Function to sort players by different metrics and plot them

--- FantasyPL.py
+++ FantasyPL.py
@@ -1,7 +1,17 @@
 import requests
+import operator
+import matplotlib.pyplot as plt
+import pandas as pd
 from ElementType import ElementType
 from Team import Team
 from Player import Player
+
+def convertTo(sort_type, player):
+  if sort_type == "ROI": # Return of Inverstment
+    return (player.total_points)*1.0/(player.cost)
+  elif sort_type == "MPP": # Minutes Per Point
+    if player.total_points > 0 and player.minutes_played >= 1000:
+      return (player.minutes_played)*1.0/(player.total_points)
 
 class FantasyPL:
   def __init__(self):
@@ -29,8 +39,8 @@ class FantasyPL:
 
       self.element_types.append(new_element_type)
 
-      attrs = vars(new_element_type)
-      print(', '.join("%s: %s" % item for item in attrs.items()))
+      # attrs = vars(new_element_type)
+      # print(', '.join("%s: %s" % item for item in attrs.items()))
 
   def createTeams(self):
     self.teams = []
@@ -64,7 +74,7 @@ class FantasyPL:
       element_type = player["element_type"]
       team_code = player["team_code"]
       minutes_played = player["minutes"]
-      cost = player["now_cost"]
+      cost = player["now_cost"]/10.0
       total_points = player["total_points"]
       bonus = player["bonus"]
       status = player["status"]
@@ -79,6 +89,36 @@ class FantasyPL:
       
       # attrs = vars(new_player)
       # print(', '.join("%s: %s" % item for item in attrs.items()))
+
+  def sortBy(self, sort_type, is_descending, num_players):
+    players_dict = {}
+    for player in self.players:
+      y_item = convertTo(sort_type, player)
+      if y_item == None:
+        continue
+      player_name = player.first_name.replace(' ', '\n') + '\n' + player.second_name.replace(' ', '\n')
+      players_dict[player_name] = y_item
+    
+    sorted_players_dict = sorted(players_dict.items(), key=operator.itemgetter(1), reverse=is_descending)
+
+    name_list = []
+    y_items_list = []
+    counter = 0
+
+    for key, value in sorted_players_dict:
+      if counter < num_players:
+        counter = counter + 1
+      else:
+        break
+      name_list.append(key)
+      y_items_list.append(value)
+
+    sorted_players_list = {"Name": name_list, sort_type: y_items_list}
+    sorted_players_data_frame = pd.DataFrame(sorted_players_list)
+    # print(sorted_players_data_frame)
+    ax = sorted_players_data_frame.plot.bar(x='Name', y=sort_type, rot=0)
+    plt.show()
+
 
   def predictSquad(self):
     print("Calling predictSquad function")
--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 from FantasyPL import FantasyPL
 
 fantasyPL = FantasyPL()
@@ -5,4 +7,10 @@ fantasyPL.getFPLJson()
 fantasyPL.createElementTypes()
 fantasyPL.createTeams()
 fantasyPL.createPlayers()
-fantasyPL.predictSquad()
+fantasyPL.sortBy("MPP", False, 10)
+# fantasyPL.predictSquad()
+
+# fields = ['first_name', 'second_name', 'cost']
+# df = pd.DataFrame([{fn: getattr(f, fn) for fn in fields} for f in fantasyPL.players[0:10]])
+# ax = df.plot.bar(x='second_name', y='cost', rot=0)
+# plt.show()
